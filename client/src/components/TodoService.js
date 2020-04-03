d7742fb8385e77b73d70361e58a0eee3ed11f5eb
Eduardo Russo <russoedu@gmail.com>
2020-04-03 11:15:19 +0100

    Client accessing node exress api

---
+++ client/src/components/TodoService.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+
+export default class TodoService {
+
+  all(callback) {
+    axios.get('http://localhost/api/')
+    .then((response) => {
+      callback(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      callback(null);
+    });
+  }
+
+  get(id,callback) {
+    axios.get('http://localhost/api/'+id)
+    .then((response) => {
+      callback(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      callback(null);
+    });
+  }
+
+  add(data,callback) {
+    axios.post('http://localhost/api/add/', {
+    desc: data
+    })
+    .then(function (response) {
+      console.log(response);
+      callback();
+    })
+    .catch(function (error) {
+      console.log(error);
+      callback();
+    });
+  }
+
+  update(data, id, callback){
+    axios.post('http://localhost/api/update/'+id, {
+      desc: data
+    })
+    .then(function(response) {
+      console.log('Updated');
+      callback();
+    })
+    .catch(function(response) {
+      callback();
+    });
+  }
+
+  delete(id, callback){
+    axios.get('http://localhost/api/delete/'+id)
+    .then(function(response){
+      callback();
+    })
+    .catch(function(response){
+      console.log('Error deleting');
+      callback();
+    });
+  }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,51 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+//index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import {Header} from "./components/Header";
+
+import AddTodo from './components/AddTodo';
+import ListTodo from './components/ListTodo';
+import UpdateTodo from './components/UpdateTodo';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div className="container">
+    <div className="row">
+      <div className="col-xs-12">
+        <Header/>
+      </div>
+    </div>
+    <div>
+      <Router>
+      <div>
+      <Route path='/add' component={AddTodo} />
+      <Route exact path='/' component={ListTodo} />
+      <Route path='/update/:id' component={UpdateTodo} />
+      </div>
+      </Router>
+    </div>
+  </div>
+,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
