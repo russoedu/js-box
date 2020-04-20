816061113194fbf49c941ddc724a0db6d7d60d74
Eduardo Russo <russoedu@gmail.com>
2020-04-20 20:27:23 +0100

    New header with env vars

--- client-angular/src/app/header/header.component.ts
+++ client-angular/src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from '../../environments/environment';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public client = '';
+  public env = '';
+  constructor() {
+    this.client = environment.JS_BOX_CLIENT;
+    this.env = environment.JS_BOX_ENVIRONMENT;
+  }
 
   ngOnInit() { }
 
--- client-react/src/components/Header.js
+++ client-react/src/components/Header.js
@@ -6,7 +6,7 @@ class App extends React.Component {
       <header className="navbar navbar-expand">
         <a className="navbar-brand" href="/">
           <img id="logo" height="75px" src="/img/JS-box.svg" alt="JS Dock logo" />
-          <span className="title">JS Box React</span>
+          <span className="title">JS Box {process.env.REACT_APP_CLIENT} - {process.env.REACT_APP_ENVIRONMENT}</span>
         </a>
       </header>
     )
