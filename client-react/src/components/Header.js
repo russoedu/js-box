b5cd0651f93d2f0fff8b67eebbe752fa4f8de13e
Eduardo Russo <russoedu@gmail.com>
2020-04-22 15:17:58 +0100

    Fixed small issue with React function name

--- client-react/src/components/Header.js
+++ client-react/src/components/Header.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 class App extends React.Component {
   render () {
+    const client = process.env.REACT_APP_CLIENT.charAt(0).toUpperCase() + process.env.REACT_APP_CLIENT.substring(1)
     return (
       <header className="navbar navbar-expand">
         <a className="navbar-brand" href="/">
           <img id="logo" height="75px" src="/img/JS-box.svg" alt="JS Box logo" />
-          <span className="title">JS Box {process.env.REACT_APP_CLIENT} - {process.env.REACT_APP_ENVIRONMENT}</span>
+          <span className="title">JS Box {client} - {process.env.REACT_APP_ENVIRONMENT}</span>
         </a>
       </header>
     )
